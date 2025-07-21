e610403b7d10c7eb52395edb37000a1e8e3f4865
haidara yousef <haidara.yousef993@gmail.com>
2025-07-21 22:21:32 +0300

    adding product model,serializer, viewset ,urls and tests

--- rest/serializers.py
+++ rest/serializers.py
@@ -1,7 +1,29 @@
 from rest_framework import serializers
 from rest_framework import viewsets
-from .models import Category
+from .models import Category, Product
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
         fields=['id','name','description','is_active']
+
+class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    
+    class Meta:
+        model = Product
+        fields = [
+            'id', 
+            'name', 
+            'description', 
+            'price', 
+            'category', 
+            'category_name',
+            'is_available', 
+            'preparation_time',
+            'created_at',
+            'updated_at'
+        ]
+        extra_kwargs = {
+            'category': {'write_only': True}
+        }
+        
